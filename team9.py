e32c0dfae1932ad2b7c4dfe6086449e2528bef24
cjvoyek <covoyek@student.linnmar.org>
2017-10-05 15:30:37 -0500

    quick addition

--- team9.py
+++ team9.py
@@ -1,10 +1,10 @@
-
-
 team_name = 'The best'
 strategy_name = 'T for T v2'
 strategy_description = 'Counters all b and all c'
     
 def move(my_history, their_history, my_score, their_score):
+    if my_score < their_score:
+        return 'b'
     if len(their_history) == 0:
         return 'b'
     elif their_history(-1) == 'b':
