41840677383f81843638c0da04e6181f11149d7d
cjvoyek <covoyek@student.linnmar.org>
2017-10-06 13:58:54 -0500

    New idea
    its an idea

--- team9.py
+++ team9.py
@@ -1,20 +1,15 @@
-team_name = 'The best'
-strategy_name = 'T for T v2'
-strategy_description = 'Counters all b and all c'
-    
+team_name = 'Vindictish'
+strategy_name = 'I shall forgeev'
+strategy_description = 'meanie gets meanie'
+import random
 def move(my_history, their_history, my_score, their_score):
-    if my_score < their_score:
-        return 'b'
-    if len(their_history) == 0:
-        return 'b'
-    elif their_history(-1) == 'b':
-        return 'b'
-    if len(their_history) == 5:
-       if their_history(-5) == 'ccccc':
+    if len(my_history) == 0:
+        return 'c'
+    elif 'b' in their_history(-3):
+        if random.random() > 0.1:
             return 'b'
-    if len(their_history) == 6:
-        if their_history(-6) == 'cccbbc':
-            return 'b'
+        else:
+            return 'c'
     else:
         return 'c'
             
