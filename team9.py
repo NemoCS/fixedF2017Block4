8b247fc24e7154f9e5e0db595b3ebd96e93e897e
cjvoyek <covoyek@student.linnmar.org>
2017-10-06 14:05:24 -0500

    a

--- team9.py
+++ team9.py
@@ -1,15 +1,14 @@
 team_name = 'Vindictish'
 strategy_name = 'I shall forgeev'
 strategy_description = 'meanie gets meanie'
-import random
+
 def move(my_history, their_history, my_score, their_score):
     if len(my_history) == 0:
         return 'c'
-    elif 'b' in their_history(-3):
-        if random.random() > 0.1:
-            return 'b'
-        else:
-            return 'c'
+    elif 'b' not in their_history(-5):
+        return 'c'
+    elif 'b' in their_history: 
+        return 'b'
     else:
         return 'c'
             
