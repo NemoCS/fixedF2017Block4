03db71c02008ce2675f5bf0d6867d363231111f4
Bryce Spencer <32553395+brycespencer@users.noreply.github.com>
2017-10-09 14:50:31 -0500

    Bryce Spencer I.P.D. Commit
    Final program for iterative prisoner's dilemma

--- team11.py
+++ team11.py
@@ -1,29 +1,36 @@
 
-team_name = 'Brick Sprencer' # Only 10 chars displayed.
-strategy_name = "DoubleCrosser*"
+team_name = 'X2X' # Only 10 chars displayed.
+strategy_name = "Double Crosser + Basic Counters"
 strategy_description = 'Changes based on # of betrayals'
     
-def move(my_history, their_history, my_score, their_score, number_of_rounds):
-    
-    their_betrayals = 0
+def move(my_history, their_history, my_score, their_score):
     
     if len(their_history) == 0:
         return 'c'
         
-    if their_history[-1] == 'b':
-        their_betrayals += 1
-
-    if len(their_history) > (number_of_rounds - 3):
-        return 'b'
-
-    if len(their_history) < number_of_rounds:
+    if len(their_history) > 99:
+        return 'b' 
+         
+    if their_history.count('b') > 2:
+        return 'b'    
+        
+    elif 'c' not in their_history:
+        return 'b'   
+        
+    elif 'b' not in their_history:
+        return 'c'     
+        
+    elif my_history[-2] == their_history[-1]:
+        return 'c' 
+        
+    elif their_history[0:4] == 'cbcb' or their_history[0:4] == 'bcbc':
+        return 'b'  
+             
+    elif len(their_history) < 100: 
         if len(their_history) > 6:
             if 'b' not in their_history[:7]:
                  return 'c'
-                 
-    if their_betrayals > 3:
-        return 'b'
-
+        
     else:
         return 'c'
         
