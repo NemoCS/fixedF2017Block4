f1b5306f4a0db1fba95ba527469cd8d8cb8fd5ec
elcagnard <32580026+elcagnard@users.noreply.github.com>
2017-10-10 15:13:31 -0500

    Team 16
    Elise Cagnard

--- team16.py
+++ team16.py
@@ -1,25 +1,38 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+team_name = "Team16"
 
-team_name = 'SneakyT4T' # Only 10 chars displayed.
-strategy_name = 'T4T but occasionally betrays randomly'
-strategy_description = 'T4T but occasionally betrays randomly'
-    
+strategy_name = "Lets see if this works"
+
+strategy_description = "Luck"
+
+import random
+
+#my_history = ""
+
 def move(my_history, their_history, my_score, their_score):
-    import random
+
     if len(their_history)==0:
-        return 'c'
-    elif their_history[-1] == 'b':
-        return 'b'
+        return random.choice([ "c ", "b" ])
+
+    elif len(their_history)==1:
+        return random.choice([ "c ", "b" ])
+
+    elif len(their_history)==2:
+        return random.choice([ "c ", "b" ])
+
+    elif len(their_history)==03:
+        return random.choice([ "c ", "b" ])
+
+    elif their_history[-3] == ( "b", "b", "b"):
+        return "c"
+
+    elif their_history[-3] == ("c", "c", "c"):
+        return "b"
+
+    elif their_history[-3] == ("b", "c", "b"):
+        return "c"
+    
+    elif my_history[-4] == ("c", "c","c", "c"):
+        return "b"
     else:
-        #Collude 90% of the time after opponent colludes
-        if random.random() < 0.90:
-            return 'c'
-        #Betray 10% of the time after opponent colludes
-        else:
-            return 'b'          
+        return "b"
+
